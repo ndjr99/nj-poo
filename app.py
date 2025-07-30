7d9adc12ee8a8e583a48f24fafc8e0f646699b6c
Your Name <you@example.com>
2025-07-30 11:26:17 -0500

    Creando mis primeras clases

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print("Hola")
+class Perro:
+    def __init__(self, nombre, raza):
+        self.nombre = nombre
+        self.raza = raza
+
+    def ladrar(self):
+        print("El perro qe esta ladranfo es: ", self.nombre)
+    
+class Persona:
+    def _init_(self, nombre, edad, sexo):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        
+#Vamos a instanciar un objeto desde la clase Perro
+print("Mi primer perrito:")
+mi_perrito = Perro("Mia", "Golden")
+print(mi_perrito.nombre)
+print(mi_perrito.raza)
+
+print("Mi segundo perrito:")
+otro_perrito = Perro("Happy", "Criolla")
+print(otro_perrito.nombre)
+print(otro_perrito.raza)
+
+nombre = input("Ingrese el nombre del perro: ")
+raza = input("Ingrese la raza del perro: ")
+tercer_perrito = Perro(nombre, raza)
+print("Tercer perrito:")
+print(tercer_perrito.nombre)
+print(tercer_perrito.raza)
+
+print("Ahora los perros van a ladrar")
+mi_perrito.ladrar()
+otro_perrito.ladrar()
+tercer_perrito.ladrar()
